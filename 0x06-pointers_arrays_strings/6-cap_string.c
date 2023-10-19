c02658c48370d32d040a40122d43614c18376600
sapiness <sapiness695@gmail.com>
2023-10-19 06:10:54 -0700

    capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: pointer to string
+ * Return: pointer to s
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	/*scan through string */
+	i = 0;
+	while (s[i] != '\0')
+	{/* if next character after count is a char, capitalize it */
+		if (s[0] >= 97 && s[0] <= 122)
+		{
+			s[0] = s[0] - 32;
+		}
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n'
+		|| s[i] == ',' || s[i] == ';' || s[i] == '.'
+		|| s[i] == '.' || s[i] == '!' || s[i] == '?'
+		|| s[i] == '"' || s[i] == '(' || s[i] == ')'
+		|| s[i] == '{' || s[i] == '}')
+		{
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+			{
+				s[i + 1] =  s[i + 1] - 32;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
